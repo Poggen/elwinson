af86f08f2ed9575a7ae93baa7f8dd793ca1bec13
Oscar Hillestad <oscarhillestad@gmail.com>
2021-02-20 23:48:10 +0100

    chore: add logo and remove boilerplate

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,10 @@ export default function Home() {
                 <nav class="relative flex items-center justify-between sm:h-10" aria-label="Global">
                   <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                     <div class="flex items-center justify-between w-full md:w-auto">
+                      <a href="#">
+                        <span class="sr-only">Workflow</span>
+                        <img class="h-8 w-auto sm:h-28" src="/logo.jpg" />
+                      </a>
                       <div class="-mr-2 flex items-center md:hidden">
                         <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                           <span class="sr-only">Öppna meny</span>
@@ -55,7 +59,7 @@ export default function Home() {
                 <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                   <div class="px-5 pt-4 flex items-center justify-between">
                     <div>
-                      <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
+                      <img class="h-8 w-auto" src="/logo.jpg" alt="Elwinson Aesthetics logo" />
                     </div>
                     <div class="-mr-2">
                       <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -68,12 +72,16 @@ export default function Home() {
                     </div>
                   </div>
                   <div class="px-2 pt-2 pb-3 space-y-1">
-                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Behandlingar</a>
+                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
 
-                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Kontakt</a>
+                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
+
+                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
+
+                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
                   </div>
                   <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
-                    Boka nu
+                    Log in
                   </a>
                 </div>
               </div>
